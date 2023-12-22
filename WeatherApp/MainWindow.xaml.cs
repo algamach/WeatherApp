f50ac1a6366207fdb7a5c270848dc35e10e0ada5
algamach <123144931+algamach@users.noreply.github.com>
2023-12-23 01:01:34 +0300

    Update project
    add a request to the api
    add try/catch
    add some nice ui

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,74 @@ namespace WeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string _KEY = "d27b83f5e1880dfe4cc0fc20525fddba";
+        private const string _ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private async void getWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            string city = cityTextbox.Text;
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("Введите название города!");
+                return;
+            }
+
+            try
+            {
+                WeatherData weatherData = await GetWeatherData(city);
+                UpdateUI(weatherData);
+            }
+            catch (Exception ex)
+            {                
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+        private async Task<WeatherData> GetWeatherData(string city)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = $"{_ENDPOINT}?q={city}&appid={_KEY}&units=metric&lang=ru";
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+                    return weatherData;
+                }
+                else
+                {
+                    throw new Exception($"Не удалось получить данные о погоде\n{response.StatusCode}");
+                }
+            }
+        }
+
+        private void UpdateUI(WeatherData weatherData)
+        {
+            cityTextblock.Text = cityTextbox.Text;            
+            descriptionTextblock.Text = $"Описание: {weatherData.Weather[0].Description}";
+            tempTextblock.Text = $"{weatherData.Main.Temperature} °C";
+            windTextblock.Text = $"{weatherData.Wind.Speed} м/с"; 
         }
     }
 }
---
+++ WeatherApp/WeatherData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherData
+    {
+        public MainInfo Main { get; set; }
+        public WeatherInfo[] Weather { get; set; }
+        public WindInfo Wind { get; set; }
+    }
+    public class MainInfo
+    {
+        [JsonProperty("temp")]
+        public float Temperature { get; set; }
+    }
+
+    public class WeatherInfo
+    {
+        public string Description { get; set; }
+    }
+
+    public class WindInfo
+    {
+        public float Speed { get; set; }
+    }
+}
